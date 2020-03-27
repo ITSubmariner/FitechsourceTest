85208ab094f1171069d8d2109a4553b6f21bef08
Anton Novikov <it.novikov.anton@yandex.ru>
2020-03-27 13:49:32 +0300

    added psvm

---
+++ src/main/java/com/fitechsource/Fitechsource.java
@@ -0,0 +1,8 @@
+package com.fitechsource;
+
+public class Fitechsource {
+    public static void main(String[] args) {
+        CustomThreadTestImpl test = new CustomThreadTestImpl();
+        test.calculate();
+    }
+}
